3c7148ef1536ca1be98f36ab794e32e24152e825
Tom Havlik <keres993@gmail.com>
2020-06-20 09:42:13 -0500

    ...

--- rust_test.py
+++
@@ -1,6 +0,0 @@
-from ctypes import *
-so_file = "target/debug/libencoder.so"
-my_functions = CDLL(so_file)
-print(type(my_functions))
-assert my_functions.mul(2, 3) == 6
-print('all good')
--- video.py
+++ video.py
@@ -1,20 +1,20 @@
-# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
-import cv2
-# initialize the camera and grab a reference to the raw camera capture
+from ctypes import ctypes
+
+so_file = "target/debug/libencoder.so"
+rav1e = ctypes.CDLL(so_file)
+
+encoder = rav1e.new_encoder(1920, 1080)
+
 resolution = (1920, 1080)
 camera = PiCamera()
 camera.resolution = resolution
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=resolution)
-# allow the camera to warmup
 time.sleep(0.1)
-# capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	image = frame.array
-	# encode the frame
-    
-	# clear the stream in preparation for the next frame
+	rav1e.encode_frame(encoder, image)
 	rawCapture.truncate(0)
