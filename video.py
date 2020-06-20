24d995203e9f5489c854226bb71cf81b1b2ab453
Tom Havlik <keres993@gmail.com>
2020-06-20 09:44:11 -0500

    ...

--- video.py
+++ video.py
@@ -12,9 +12,9 @@ resolution = (1920, 1080)
 camera = PiCamera()
 camera.resolution = resolution
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=resolution)
+raw_capture = PiRGBArray(camera, size=resolution)
 time.sleep(0.1)
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
 	image = frame.array
 	rav1e.encode_frame(encoder, image)
-	rawCapture.truncate(0)
+	raw_capture.truncate(0)
