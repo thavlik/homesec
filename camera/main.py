8af555f74983cdb881d3082a12142370c49a51f1
Tom Havlik <keres993@gmail.com>
2020-06-20 22:30:13 -0500

    ...

--- camera/main.py
+++ camera/main.py
@@ -12,13 +12,13 @@ parser.add_argument('--height', type=int, default=1080, metavar='h',
                     help='vertical resolution (in pixels)')
 parser.add_argument('--frame-rate', type=int, default=30, metavar='r',
                     help='frames per second')
-parser.add_argument('--util-path', type=str,
+parser.add_argument('--lib-path', type=str,
                     default=os.path.normpath(os.path.join(
-                        os.path.realpath(__file__), "../target/debug/libutil.so")),
-                    help='path to libutil.so')
+                        os.path.realpath(__file__), "../target/debug/libcamera.so")),
+                    help='path to libcamera.so')
 args = parser.parse_args()
 
-util = ctypes.CDLL(args.util_path)
+util = ctypes.CDLL(args.lib_path)
 
 #encoder = util.new_encoder(args.width, args.height)
 
