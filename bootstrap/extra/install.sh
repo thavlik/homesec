ff4f77304a424b1c57db70b66854e4758de5749a
Tom Havlik <keres993@gmail.com>
2020-06-26 17:11:07 -0500

    ...

--- bootstrap/extra/install.sh
+++ bootstrap/extra/install.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 cd "$(dirname "$0")"/..
 cargo build
-sudo cp ../target/debug/homesec-bootstrap /usr/bin/homesec-bootstrap
+sudo cp ../../target/debug/homesec-bootstrap /usr/bin/homesec-bootstrap
 sudo cp ./homesec-bootstrap.service /etc/systemd/system/homesec-bootstrap.service
 sudo systemctl start homesec-bootstrap
