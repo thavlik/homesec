d76ebce829a68d798e6871b996fca295a93d9687
Tom Havlik <keres993@gmail.com>
2020-06-26 20:28:31 -0500

    ...

--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -1,25 +1,36 @@
 use std::io;
 use std::net::UdpSocket;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 struct AppearanceMessage {
+    address: [u8; 4],
+    is_master: bool,
 }
 
 fn main() -> io::Result<()> {
-    let mut socket = UdpSocket::bind("0.0.0.0:43000")?;
+    let port = 43000;
+    let broadcast_addr = format!("192.168.0.255:{}", port);
+    let mut socket = UdpSocket::bind(format!("0.0.0.0:{}", port))?;
     socket.set_nonblocking(true)?;
     socket.set_broadcast(true)?;
     loop {
         let mut buf = [0; 10];
         match socket.recv_from(&mut buf) {
             Ok(n) => {
-                println!("got message");
+                let msg: AppearanceMessage = bincode::deserialize(&buf[..]).expect("deserialize");
+                println!("{:?}", msg);
             },
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
             }
             Err(e) => panic!("encountered IO error: {}", e),
         }
-        socket.send_to(&[0; 10], "192.168.0.255:43000")?;
-        println!("sent packet");
+        let msg = AppearanceMessage{
+            is_master: false,
+            address: [0, 0, 0, 0], 
+        };
+        let encoded: Vec<u8> = bincode::serialize(&msg).expect("serialize");
+        socket.send_to(&encoded[..], &broadcast_addr)?;
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
     Ok(())
