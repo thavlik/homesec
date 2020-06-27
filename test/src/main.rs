ee9ae8baef20c9f13b462613b11edd563263ddb5
Tom Havlik <keres993@gmail.com>
2020-06-27 08:31:08 -0500

    ...

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,10 +1,15 @@
-fn main() {
+use std::net::SocketAddr;
+use anyhow::Result;
 
+fn get_addresses() -> Result<Vec<SocketAddr>> {
+    Ok(vec!["192.168.0.102".parse().unwrap()])
 }
 
-#[cfg(test)]
-mod test {
-    #[test]
-    fn foobar() {
-    }
+fn basic_daemon_install() -> Result<()> {
+    let addresses = get_addresses()?;
+    Ok(())
+}
+
+fn main() {
+
 }
