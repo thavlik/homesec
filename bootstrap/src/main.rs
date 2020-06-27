8cc9afc6602edb365331892fe001037f3a6dee1b
Tom Havlik <keres993@gmail.com>
2020-06-26 20:17:05 -0500

    ...

--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -1,57 +1,34 @@
+use std::io;
+use std::net::UdpSocket;
 
-use pnet::datalink::{self, NetworkInterface};
-use pnet::datalink::Channel::Ethernet;
-use pnet::packet::{Packet, MutablePacket};
-use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
+struct AppearanceMessage {
+}
 
-use std::env;
-
-fn main() {
-    let interface_name = env::args().nth(1).unwrap();
-    let interface_names_match =
-        |iface: &NetworkInterface| iface.name == interface_name;
-
-    // Find the network interface with the provided name
-    let interfaces = datalink::interfaces();
-    let interface = interfaces.into_iter()
-                              .filter(interface_names_match)
-                              .next()
-                              .unwrap();
-
-    // Create a new channel, dealing with layer 2 packets
-    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
-    };
-
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    //let mut socket = UdpSocket::bind("0.0.0.0:34254")?;
+    //tokio::spawn(async move {
+    //    loop {
+    //        let mut buf = [0; 10];
+    //        let (amt, src) = socket.recv_from(&mut buf)?;
+    //        println!("Received packet");
+    //    }
+    //});
+    let mut socket = UdpSocket::bind("0.0.0.0:43000")?;
+    socket.set_nonblocking(true)?;
     loop {
-        match rx.next() {
-            Ok(packet) => {
-                let packet = EthernetPacket::new(packet).unwrap();
-
-                // Constructs a single packet, the same length as the the one received,
-                // using the provided closure. This allows the packet to be constructed
-                // directly in the write buffer, without copying. If copying is not a
-                // problem, you could also use send_to.
-                //
-                // The packet is sent once the closure has finished executing.
-                tx.build_and_send(1, packet.packet().len(),
-                    &mut |mut new_packet| {
-                        let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
-
-                        // Create a clone of the original packet
-                        new_packet.clone_from(&packet);
-
-                        // Switch the source and destination
-                        new_packet.set_source(packet.get_destination());
-                        new_packet.set_destination(packet.get_source());
-                });
+        let mut buf = [0; 10];
+        match socket.recv_from(&mut buf) {
+            Ok(n) => {
+                println!("got message");
             },
-            Err(e) => {
-                // If an error occurs, we can handle it here
-                panic!("An error occurred while reading: {}", e);
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
             }
+            Err(e) => panic!("encountered IO error: {}", e),
         }
+        socket.send_to(&[0; 10], "192.168.0.255:43000")?;
+        println!("sent packet");
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
+    Ok(())
 }
