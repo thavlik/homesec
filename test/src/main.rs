f65ffec19222151968514c5ad564d2d49f853e49
Tom Havlik <keres993@gmail.com>
2020-06-27 08:54:19 -0500

    ...

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,5 +1,18 @@
 use std::net::SocketAddr;
 use anyhow::Result;
+use std::process::Command;
+use std::io::{self, Write};
+
+fn build_for_arm() -> Result<()> {
+    let output = Command::new("cargo")
+        .args(&["build", "--target", "armv7-unknown-linux-gnueabihf"])
+        .current_dir("../bootstrap")
+        .output()
+        .expect("build failed");
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+    Ok(())
+}
 
 fn get_addresses() -> Result<Vec<SocketAddr>> {
     Ok(vec!["192.168.0.102".parse().unwrap()])
@@ -11,5 +24,5 @@ fn basic_daemon_install() -> Result<()> {
 }
 
 fn main() {
-
+    build_for_arm().unwrap();
 }
